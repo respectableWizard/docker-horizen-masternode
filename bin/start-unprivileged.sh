baf37cb915e1778ff4ff304c290c75ed29c070d9
respectaWiz <respectableWizard@gmail.com>
2020-07-23 15:07:08 +1000

    Extend Zen configuration
    Add rpcconnect parameter. Will use that to connect to the instance
    Add rpcallowip to lock down the configuration
    Make sure RPC server is enabled
    Update path to the certification

--- bin/start-unprivileged.sh
+++ bin/start-unprivileged.sh
@@ -17,11 +17,12 @@ if [ ! -e "$FILE" ]; then
 printtoconsole=${PRINTTOCONSOLE:-1}
 externalip=${EXTERNALIP}
 rpcbind=$(hostname -I | awk '{print $1}')
-rpcallowip=$(hostname -I | awk '{print $1}')
+rpcconnect=${RPCCONNECT:-$(hostname -I | awk '{print $1}')}
+rpcallowip=${RPCALLOWIP:-$(hostname -I | awk '{print $1}')}
 server=1
 masternode=1
 daemon=0
-listen=0
+listen=1
 txindex=1
 logtimestamps=1
 rpcuser=${RPCUSER:-horizenrpc}
@@ -29,17 +30,12 @@ rpcpassword="${RPCPASSWORD:-`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32
 rpcport=${RPCPORT:-18231}
 port=${PORT:-9033}
 rpcworkqueue=512
-tlscertpath=/etc/letsencrypt/live/${FQDN}/cert.pem
-tlskeypath=/etc/letsencrypt/live/${FQDN}/privkey.pem
+tlscertpath=${DIR}/letsencrypt/live/${FQDN}/cert.pem
+tlskeypath=${DIR}/letsencrypt/live/${FQDN}/privkey.pem
 EOF
 
 fi
-
-
-
 cat $FILE
 ls -lah $DIR/
-
 echo "Initialization completed successfully"
-
 zen-fetch-params && exec $EXECUTABLE
