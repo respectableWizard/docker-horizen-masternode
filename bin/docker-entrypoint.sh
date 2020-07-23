6361eb7f0ff4eab7949a9185926eee559324ffe5
respectaWiz <respectableWizard@gmail.com>
2020-07-23 15:04:30 +1000

    Fail if an error is detected

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,9 +5,9 @@ USER=horizen
 DIR=$HOME/.zen
 
 mkdir -p ${DIR}/letsencrypt/live
-cp /etc/letsencrypt/live/${FQDN}/cert.pem ${DIR}/letsencrypt/live
-cp /etc/letsencrypt/live/${FQDN}/privkey.pem ${DIR}/letsencrypt/live
-update-ca-certificates
-
-chown -R ${USER} .
+cp /etc/letsencrypt/live/${FQDN}/cert.pem ${DIR}/letsencrypt/live && \
+cp /etc/letsencrypt/live/${FQDN}/privkey.pem ${DIR}/letsencrypt/live && \
+cp /etc/letsencrypt/live/${FQDN}/chain.pem /usr/local/share/ca-certificates/chain.crt && \
+update-ca-certificates && \
+chown -R ${USER} . && \
 exec gosu ${USER} "$@"
